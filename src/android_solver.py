c9656daf2ccc3022fe2677c1129efdc49632a996
Dan Freeman <dan4am@gmail.com>
2021-06-29 03:27:40 +0200

    minor refactoring

--- src/android_solver.py
+++ src/android_solver.py
@@ -1,85 +1,28 @@
 import sys
 sys.path.append("..")
+from src.image_processing import *
 from src.solver import *
-import numpy as np
-import cv2
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+from ppadb.client import Client
 
-thickness =3
-width = 106
-height = 106
-top_left_x = 48
-top_left_y = 385
-
-selector_centering = 50
-selector_top_left_x = 120
-selector_top_left_y = 1503
-
-selector_width = 192
-selector_height = 108
-
-#crop the image at the corresponding coordinates in the grid
-
-def read_number(array,x,y):
-    result = []
-    for i in range (top_left_y + (thickness * y)+(height*(y-1)), top_left_y + (thickness *y )+(height*(y-1))+height):
-        result.append([])
-        for j in range (top_left_x+ (thickness * x) +(width * (x-1)), top_left_x+(width * (x-1))+ (thickness * x)+width):
-            result[-1].append (array[i][j])
-    return np.array(result)
+def connect_device():
+    adb = Client(host='127.0.0.1',port=5037)
+    devices = adb.devices()
+    # print(devices)
+    if len(devices) == 0:
+        print("No Devices Attached")
+        quit()
+    return devices[0]
 
 
-
-#read the number in the cropped image and transform it into an int
-
-def from_pic_to_number(pic):
-    result = pytesseract.image_to_string(pic, config='--psm 10')
-    result = result[0]
-    if result == 'A':
-        result = 4
-    elif result == '_':
-        result  = 0
-    else:
-        result = int(result)
-    return result
-
-
-
-#transform screenshot into a 2d array
-
-def from_img_to_array(img):
-    result = []
-    for y in range (1,10):
-        result.append([])
-        for x in range(1,10):
-            pic = read_number(img,x,y)
-            result[-1].append(from_pic_to_number(pic))
-    return result
-
-#reads an image and transforms it into black and white image (0=black, 255=white)
-
-def read_img_bw(path):
-    img = cv2.imread(path, 0)
-    for i in range (len(img)):
-        for j in range( len (img[0])):
-            if (img[i][j] < 150):
-                img[i][j] = 0
-            else:
-                img[i][j] = 255
-
-    # cv2.imwrite("test2.png", img)
-
-    return img
-
-
-
-
+def take_screenshot(device):
+    image = device.screencap()
+    with open('../gui/screen1.png', 'wb') as f:
+        f.write(image)
+    # return image
 def tap(device,x,y):
     x1 = top_left_x+ (thickness * x) + (width * (x-1)) + 4
     y1 = top_left_y + (thickness * y) + (height*(y-1)) + 4
     device.shell('input tap ' + str(x1)+" "+ str(y1))
-
 
 def select_number(device, number):
     x = (number-1) % 3 + 1
@@ -89,8 +32,6 @@ def select_number(device, number):
     y1 = selector_top_left_y + (selector_centering * y) + (selector_height*(y-1)) + 4
 
     device.shell('input tap ' + str(x1) + " " + str(y1))
-
-
 
 def solve_by_column(device):
 
@@ -194,26 +135,21 @@ def solve_by_line(device):
 
 
 def main():
-    pass
-    # img = read_img_bw('screen.png')
-    # test = from_img_to_array(img)
-    # for line in test:
-    #     print (line)
 
-    # device = connect_device()
-    # take_screenshot(device)
-    # # device.shell('input tap 600 455')
-    # img = read_img_bw('../gui/screen1.png')
-    # array = from_img_to_array(img)
-    # # for line in array:
-    # #     print (line)
-    # copy_board(array)
-    # solve_by_column(device)
-    # solve_by_square(device)
-    # solve_by_line(device)
-    # solve_by_column(device)
-    # solve_by_square(device)
-    # solve_by_line(device)
+    device = connect_device()
+    take_screenshot(device)
+    # device.shell('input tap 600 455')
+    img = read_img_bw('../gui/screen1.png')
+    array = from_img_to_array(img)
+    # for line in array:
+    #     print (line)
+    copy_board(array)
+    solve_by_column(device)
+    solve_by_square(device)
+    solve_by_line(device)
+    solve_by_column(device)
+    solve_by_square(device)
+    solve_by_line(device)
 
     # select_number(device,1)
     # cv2.imshow("window_name", img)
